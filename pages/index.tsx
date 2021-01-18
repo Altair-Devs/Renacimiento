00d4dc0210af097b889d55c64e6d5a5c6877cfad
Irungaray <lionelirungaray@gmail.com>
2021-01-18 15:36:59 -0300

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>Renaissance</div>
+    </div>
+  )
+}
+
+export default HomePage
