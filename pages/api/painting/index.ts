5d26cb74ecb6f65ba8c85b9d822b83739c45b301
Irungaray <lionelirungaray@gmail.com>
2021-01-18 18:43:32 -0300

    Fetched DB

---
+++ pages/api/painting/index.ts
@@ -0,0 +1,15 @@
+import { IncomingMessage, ServerResponse } from "http";
+
+import DB from '@database';
+
+const allCards = async (request: IncomingMessage, response: ServerResponse) => {
+  const db = new DB();
+  const allEntries = await db.getAll();
+  const length = allEntries.length;
+
+  response.statusCode = 200;
+  response.setHeader( 'Content-type', 'application/json' );
+  response.end(JSON.stringify({data: allEntries, length}));
+};
+
+export default allCards;
