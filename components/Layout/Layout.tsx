40ccfb3e0b7d63eb0b9be748baf01947ff38760a
Irungaray <lionelirungaray@gmail.com>
2021-01-19 07:02:19 -0300

    Path aliased

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Navbar from '../Navbar/Navbar';
-import Footer from '../Footer/Footer';
+import Navbar from '@components/Navbar/Navbar';
+import Footer from '@components/Footer/Footer';
 
 const Layout: React.FC = ({ children }) => {
   return (
