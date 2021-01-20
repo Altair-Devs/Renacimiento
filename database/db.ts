b593122fc2cade34084022a72e2586ffb40f1c0c
Irungaray <lionelirungaray@gmail.com>
2021-01-20 05:25:41 -0300

    Fixed DB Types

--- database/db.ts
+++ database/db.ts
@@ -6,13 +6,13 @@ import allData from './data';
 class Database {
 	constructor() {}
 
-	async getAll(): Promise<TProduct[]> {
+	async getAll(): Promise<TPainting[]> {
 		const asArray = Object.values(allData);
 		await randomDelay();
 		return asArray;
 	}
 
-	async getById(id: string): Promise<TProduct | null> {
+	async getById(id: string): Promise<TPainting | null> {
 		if (!Object.prototype.hasOwnProperty.call(allData, id)) {
 			return null;
 		}
