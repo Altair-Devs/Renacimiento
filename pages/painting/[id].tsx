d598fc9c471c147da07a770ab6420e7825eda867
Irungaray <lionelirungaray@gmail.com>
2021-01-20 18:03:30 -0300

    Static Painting Details

--- pages/painting/[id].tsx
+++ pages/painting/[id].tsx
@@ -1,22 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { GetStaticProps } from 'next';
 
 import PaintingSummary from '@components/PaintingSummary/PaintingSummary';
 
-const PaintingPage = () => {
-	const { query } = useRouter();
-	const [painting, setPainting] = useState<TPainting | null>(null)
+export const getStaticPaths =  async () => {
+	const response = await fetch('https://renacimiento.altair-devs.vercel.app/api/painting')
+	const { data: paintingList }: TAPIResponse = await response.json()
 
-	useEffect(() => {
-		if (query.id) {
-			window
-				.fetch(`/api/painting/${query.id}`)
-				.then((response) => response.json())
-				.then((data: TPainting) => {
-					setPainting(data)
-				})
+	const paths = paintingList.map((paints) => ({
+		params: {
+			id: paints.id
 		}
-	}, [query.id])
+	}))
+
+	return {
+		paths: paths,
+		fallback: false
+	}
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+	const id = params?.id as string
+
+	const response = await fetch(
+		`https://renacimiento.altair-devs.vercel.app/api/painting/${id}`
+	)
+
+	const painting: TPainting = await response.json()
+
+	return {
+		props: {
+			painting,
+		}
+	}
+}
+
+const PaintingPage = ({ painting }: { painting: TPainting }) => {
 
 	return (
 		<section>
